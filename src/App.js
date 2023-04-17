2c585a3faeb1c04f34a27f612126d9a88d78e5e0
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-18 00:01:43 +0530

    create  user interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0ab8471aa18990f9f661355708e07748`
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter'){
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('');
+    }
+  }
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Colombo</p>
+          </div>
+          <div className="temp">
+            <h1>60&deg;F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65&deg;F</p>
+            <p>Fells Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
